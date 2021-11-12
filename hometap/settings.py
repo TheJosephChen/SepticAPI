9dce7580be9cf3b950c665a7d4774867ab616386
TheJosephChen <violuminescence@gmail.com>
2021-11-11 19:51:21 -0500

    Update settings.py

--- hometap/settings.py
+++ hometap/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qnkj=x$u1_fa%$+94rgiqri*%@#kjm$f=@olv3^ou)qc!jvl5='
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
